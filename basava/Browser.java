d38749a8fcc2ed58a70e482743415a5e1eecf030
vegiharish <vegi.harish@gmail.com>
2017-05-26 05:52:24 +0000

    hai

--- basava/Browser.java
+++ basava/Browser.java
@@ -1,16 +1,5 @@
-import java.awt.Desktop;
-import java.net.URI;
-
-// ...
-class Browser{
-public static void main(String[] args){
-if(Desktop.isDesktopSupported())
-{
-  Desktop.getDesktop().browse(new URI("http://www.google.com"));
-}
-else
-{
-System.out.println("code failure");
-}
-}
-}
+class Browser{  
+        public static void main(String args[]){  
+         System.out.println("Hello Java");  
+        }  
+    }  
